2b62f301ae0b09182546b882327f0626b151af7b
David Tran <davidtranhq@gmail.com>
2022-08-16 18:15:06 -0400

    Add Wallet Adapter

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,65 +1,15 @@
-import React from 'react';
+import WalletAdapter from './components/WalletAdapter'; 
 import {
-  Program,
-  AnchorProvider,
-  web3,
-} from '@project-serum/anchor'
-import {
-  Connection,
-  PublicKey
-} from '@solana/web3.js'
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
 
-import { Whitelist } from './lib/whitelist';
-import idl from './lib/idl';
-
-declare global {
-  interface Window { solana: any; }
+export default function Home() {
+  return <>
+    <WalletAdapter>
+      <WalletMultiButton />
+      <WalletDisconnectButton />
+      <div>Hello, world!</div>
+    </WalletAdapter>
+  </>
 }
-
-// the address of the on-chain program
-const programID = new PublicKey("FRs2XJmvEULEm4X1Y17nZ8rmKMU7R1EqJVLMZxCTez6Q")
-
-function Home() {
-  const [program, setProgram] = React.useState<Program<Whitelist>>();
-
-  React.useEffect(() => {
-    // initialize the connection to the program on load
-    if (window.solana === undefined) {
-      console.log("Install a wallet manager extension such as 'Phantom'!");
-      return;
-    }
-    window.solana.on("connect", () => {
-      const wallet = window.solana
-      const network = "http://127.0.0.1:8899"
-      const connection = new Connection(network);
-
-      const providerOpts = {};
-      const provider = new AnchorProvider(
-        connection,
-        wallet,
-        providerOpts,
-      ); 
-
-      setProgram(new Program<Whitelist>(idl as Whitelist, programID, provider));
-    })
-    return () => {
-      window.solana.disconnect();
-    }
-  }, [])
-
-  async function getWallet() {
-    try {
-      const wallet = typeof window !== 'undefined' && window.solana;
-      await wallet.connect()
-    } catch (err) {
-      console.log('err: ', err)
-    }
-  }
-  return (
-    <div className="App">
-      Hello, world!
-    </div>
-  );
-}
-
-export default Home;
